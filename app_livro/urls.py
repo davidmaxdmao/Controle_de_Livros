b9abf70c77c9f2e881cfb323e42f5977beb4a50f
root <davidmax.dmao@gmail.com>
2019-10-15 21:01:00 -0300

    Foi adicionada a funcionalidade de cadastro de usuário

--- app_livro/urls.py
+++ app_livro/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import *
+from . import views
+
 
 
 urlpatterns = [
 
     path('', listar_livros, name='listar_livros'),
-    #path('livros/', listar_livros, name='listar_livros'),
     path('cadastrar_livro/', cadastrar_livro, name='cadastrar_livro'),
     path('editar_livro/<int:id>', editar_livro, name='editar_livro'),
     path('remover_livro/<int:id>', remover_livro, name='remover_livro'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+
 
 ]
 
--- app_livro/views.py
+++ app_livro/views.py
@@ -3,6 +3,14 @@ from django.contrib.auth.decorators import login_required
 from .forms import LivroForm
 from .services import livro_services
 from .entidade.livro import Livro
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
 
 @login_required
 def listar_livros(request):
